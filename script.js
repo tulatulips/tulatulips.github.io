2758eee82fe363b8502635364ee4e6a7ec35a778
tulatulips <136732497+tulatulips@users.noreply.github.com>
2023-06-15 12:06:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMore(){
+	let aboutMe=document.getElementById("about-me");
+	aboutMe.classList.remove("hidden1");
+}
+
+function showMore2(){
+	let futurePlans=document.getElementById("future-plans");
+	futurePlans.classList.remove("hidden2");
+}
